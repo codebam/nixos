5aee55486ffbbaf346341ef8906cb1c853ad0266
Sean Behan <codebam@riseup.net>
2024-04-11 09:09:29 -0400

    add configurations

---
+++ configuration.nix
@@ -0,0 +1,108 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
+  networking.wireless.iwd.enable = true;
+
+  time.timeZone = "America/Toronto";
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  users.users.codebam = {
+    isNormalUser = true;
+    home = "/home/codebam";
+    description = "Sean Behan";
+    extraGroups = ["wheel" "networkmanager"];
+    packages = with pkgs; [
+      flatpak
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    rcm
+    grim
+    slurp
+    wl-clipboard
+    mako
+    waybar
+    wofi
+    nodejs
+    nodePackages.wrangler
+    tmux
+    weechat
+    xdg-utils
+    (pass.withExtensions (subpkgs: with subpkgs; [
+      pass-audit
+      pass-otp
+      pass-genphrase
+    ]))
+  ];
+  fonts.packages = with pkgs; [
+    fira-code
+    fira-code-symbols
+  ];
+
+  xdg = {
+    autostart.enable = true;
+    portal = {
+      enable = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal
+        pkgs.xdg-desktop-portal-gtk
+      ];
+      wlr.enable = true;
+    };
+  };
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+
+  services.flatpak.enable = true;
+  services.udisks2.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+  };
+
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  nixpkgs.config.allowUnfree = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+  };
+
+  programs.corectrl = {
+    enable = true;
+    gpuOverclock.enable = true;
+    gpuOverclock.ppfeaturemask = "0xffffffff";
+  };
+
+  system.stateVersion = "23.11";
+}
