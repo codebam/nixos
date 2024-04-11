31bba31bbcc27a015f9d6e794ad068785cbfa327
Sean Behan <codebam@riseup.net>
2024-04-11 10:39:42 -0400

    add polkit

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,24 @@
 
   time.timeZone = "America/Toronto";
 
+  security.polkit.enable = true;
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+    };
+  };
+
+
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
