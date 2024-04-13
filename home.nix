c6ebc7b3acbcd5c5bc1432d8b803d7dc7035e32c
Sean Behan <codebam@riseup.net>
2024-04-13 10:50:43 -0400

    add tmux config

--- home.nix
+++ home.nix
@@ -125,6 +125,27 @@
   };
   programs.tmux = {
     enable = true;
+    extraConfig = ''
+      set -g default-terminal "tmux-256color"
+      set -ga terminal-overrides ",*256col*:Tc"
+      set -g @plugin 'tmux-plugins/tpm'
+      set -g @plugin 'tmux-plugins/tmux-sensible'
+      set -g @plugin 'tmux-plugins/tmux-resurrect'
+      unbind C-b
+      set -g prefix C-a
+      bind C-a send-prefix
+      bind-key C-a last-window
+      bind-key a send-prefix
+      bind-key b set status
+      bind s split-window -v
+      bind v split-window -h
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+      set -g mouse
+      run '~/.tmux/plugins/tpm/tpm'
+    '';
   };
   programs.waybar = {
     enable = true;
