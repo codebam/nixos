daedd4e734e176929c3f16dcd575821e37823765
Sean Behan <codebam@riseup.net>
2024-04-14 15:13:35 -0400

    use testing kernel

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_testing;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.gc = {
