2a4f0164209ee66f76d20a753cc85bf9dbaa2a9a
Sean Behan <codebam@riseup.net>
2024-04-22 23:09:42 -0400

    localize variables to sway

--- home.nix
+++ home.nix
@@ -1,16 +1,15 @@
 { config, pkgs, lib, ... }:
 
-let
-  wallpaper = builtins.fetchurl {
-    url = "https://images.hdqwalls.com/download/1/beach-seaside-digital-painting-4k-05.jpg";
-    sha256 = "2877925e7dab66e7723ef79c3bf436ef9f0f2c8968923bb0fff990229144a3fe";
-  };
-  modifier = "Mod4";
-in
 {
   home.username = "codebam";
   home.homeDirectory = "/home/codebam";
-  wayland.windowManager.sway = {
+  wayland.windowManager.sway = let
+    wallpaper = builtins.fetchurl {
+      url = "https://images.hdqwalls.com/download/1/beach-seaside-digital-painting-4k-05.jpg";
+      sha256 = "2877925e7dab66e7723ef79c3bf436ef9f0f2c8968923bb0fff990229144a3fe";
+    };
+    modifier = "Mod4";
+  in {
     enable = true;
     systemd.enable = true;
     config = rec {
