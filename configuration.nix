7b40f0faee4dcddc98f34925c5ca2fb697570554
Sean Behan <codebam@riseup.net>
2024-04-24 11:32:06 -0400

    switch to zen kernel

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.supportedFilesystems = [ "bcachefs" ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
