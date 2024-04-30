003608de09d6431cf4d61eb16e5ed4b94769399a
Sean Behan <codebam@riseup.net>
2024-04-29 22:14:54 -0400

    switch to testing kernel

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = pkgs.linuxPackages_testing;
   boot.supportedFilesystems = [ "bcachefs" ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
