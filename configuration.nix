3d239a8d2fc9433465268ecce8d9329d8f56b60b
Sean Behan <codebam@riseup.net>
2024-05-10 04:25:04 -0400

    configure iwd

--- configuration.nix
+++ configuration.nix
@@ -33,7 +33,24 @@
       enable = true;
       wifi.backend = "iwd";
     };
-    wireless.iwd.enable = true;
+    wireless.iwd = {
+      enable = true;
+      settings = {
+        General = {
+          RoamThreshold = -75;
+          RoamThreshold5G = -80;
+        };
+        Scan = {
+          DisablePeriodicScan = true;
+        };
+        IPv6 = {
+          Enabled = true;
+        };
+        Settings = {
+          AutoConnect = true;
+        };
+      };
+    };
     nftables.enable = true;
     firewall = {
       enable = true;
