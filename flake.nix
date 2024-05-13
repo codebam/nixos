339a0cab7a8ca9f7819b616b1c2597376807d182
Sean Behan <codebam@riseup.net>
2024-05-13 16:59:44 -0400

    switch to latest kernel

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     catppuccin.url = "github:catppuccin/nix?rev=ccc188e244e8fb3248e3c8f19f70280076bf1408";
     gen-nvim-latest-pin.url = "github:codebam/nixpkgs/gen-nvim";
-    linux-testing-update.url = "github:codebam/nixpkgs/linux_testing";
-    linux-latest-update.url = "github:K900/nixpkgs/kernel-6.9";
+    # linux-testing-update.url = "github:codebam/nixpkgs/linux_testing";
+    # linux-latest-update.url = "github:K900/nixpkgs/kernel-6.9";
     bcachefs-fix.url = "github:tmuehlbacher/bcachefs-tools/fix-encrypted-unlock";
   };
 
