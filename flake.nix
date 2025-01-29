aeaa76ab1b2b144a46f933ee421c1ae8b6ea313d
Sean Behan <codebam@riseup.net>
2025-01-29 18:32:35 -0500

    add back home manager

--- flake.nix
+++ flake.nix
@@ -51,6 +51,13 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.users.codebam = {
+            imports = [
+              ./home.nix
+              ./desktop/home.nix
+              catppuccin.homeManagerModules.catppuccin
+            ];
+          };
         }
       ];
     };
