39184cc5abdc5adbf54b9b338cf7b79ff493411a
Sean Behan <codebam@riseup.net>
2025-01-29 18:27:00 -0500

    update

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    # staging-next.url = "github:nixos/nixpkgs/staging-next";
     # master.url = "github:nixos/nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -12,19 +13,27 @@
       url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    lix = {
+      url = "git+https://git.lix.systems/lix-project/lix.git";
+      flake = false;
+    };
+    lix-module = {
+      url = "git+https://git.lix.systems/lix-project/nixos-module.git";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
+    };
     catppuccin.url = "github:catppuccin/nix";
-    # staging-next.url = "github:nixos/nixpkgs/staging-next";
     flatpak-stable.url = "github:getchoo-contrib/nixpkgs/pkgs/flatpak/1.16.0";
-    flatpak-mine.url = "github:codebam/nixpkgs/flatpak-1.16.0-old";
   };
 
-  outputs = { nixpkgs, home-manager, catppuccin, lanzaboote, ... }@inputs: {
+  outputs = { nixpkgs, home-manager, catppuccin, lanzaboote, lix-module, lix, ... }@inputs: {
     nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
         ./desktop/configuration.nix
+        lix-module.nixosModules.default
         catppuccin.nixosModules.catppuccin
         lanzaboote.nixosModules.lanzaboote
         ({ pkgs, lib, ... }: {
@@ -42,13 +51,6 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.extraSpecialArgs = { inherit inputs; };
-          home-manager.users.codebam = {
-            imports = [
-              ./home.nix
-              ./desktop/home.nix
-              catppuccin.homeManagerModules.catppuccin
-            ];
-          };
         }
       ];
     };
