08123aba67e9057fd087f8b4db1ee5ad3cb6224a
Sean Behan <codebam@riseup.net>
2025-03-31 01:26:44 -0400

    avante.nvim setup

--- home.nix
+++ home.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   lib,
+  inputs,
   ...
 }:
 
@@ -906,33 +907,6 @@
       extraLuaPackages = ps: [ ps.jsregexp ];
       extraLuaConfig = ''
 
-        require('gen').setup({
-            model = "qwen2.5-coder:32b", -- The default model to use.
-            quit_map = "q", -- set keymap to close the response window
-            retry_map = "<c-r>", -- set keymap to re-send the current prompt
-            accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
-            host = "localhost", -- The host running the Ollama service.
-            port = "11434", -- The port on which the Ollama service is listening.
-            display_mode = "split", -- The display mode. Can be "float" or "split" or "horizontal-split".
-            show_prompt = true, -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
-            show_model = true, -- Displays which model you are using at the beginning of your chat session.
-            no_auto_close = true, -- Never closes the window automatically.
-            file = false, -- Write the payload to a temporary file to keep the command short.
-            hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
-            init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
-            -- Function to initialize Ollama
-            command = function(options)
-                local body = {model = options.model, stream = true}
-                return "curl --silent --no-buffer -X POST http://" .. options.host .. ":" .. options.port .. "/api/chat -d $body"
-            end,
-            -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-            -- This can also be a command string.
-            -- The executed command must return a JSON object with { response, context }
-            -- (context property is optional).
-            -- list_models = '<omitted lua function>', -- Retrieves a list of model names
-            result_filetype = "markdown", -- Configure filetype of the result buffer
-            debug = false -- Prints errors and the command which is run.
-        })
         require('nvim-treesitter.configs').setup {
           auto_install = false,
           ignore_install = {},
@@ -1026,6 +1000,20 @@
             { name = 'luasnip' },
           },
         }
+        require("avante_lib").load()
+        require("avante").setup({
+          provider = "ollama",
+          ollama = {
+            model = "qwen2.5-coder:32b",
+          },
+          rag_service = {
+            enabled = true,
+            host_mount = os.getenv("HOME"),
+            provider = "ollama",
+            llm_model = "gemma3:27b",
+            embed_model = "nomic-embed-text",
+          },
+        })
       '';
       extraConfig = ''
         set guicursor=n-v-c-i:block
@@ -1044,9 +1032,9 @@
         set ts=2
         set undofile
         set undodir=$HOME/.vim/undodir
-        let g:augment_workspace_folders = ['~/git/seanbehan.ca', '~/git/cf-workers-telegram-bot']
       '';
       plugins = [
+        inputs.avante.legacyPackages.${pkgs.system}.vimPlugins.avante-nvim
         pkgs.vimPlugins.catppuccin-vim
         pkgs.vimPlugins.cmp_luasnip
         pkgs.vimPlugins.cmp-nvim-lsp
@@ -1068,8 +1056,6 @@
         pkgs.vimPlugins.surround
         pkgs.vimPlugins.todo-comments-nvim
         pkgs.vimPlugins.nvim-treesitter.withAllGrammars
-        pkgs.vimPlugins.augment-vim
-        pkgs.vimPlugins.gen-nvim
       ];
     };
     # vim = {
