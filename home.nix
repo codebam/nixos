405d6dd383570e3bf8651190c75abd1a0c3eb1d0
Sean Behan <codebam@riseup.net>
2025-05-13 07:10:32 -0400

    remove github token

--- home.nix
+++ home.nix
@@ -252,7 +252,6 @@
         MANGOHUD_CONFIGFILE = "/home/codebam/.config/MangoHud/MangoHud.conf";
         PROTON_ENABLE_WAYLAND = "1";
         DXVK_HDR = "1";
-        GITHUB_TOKEN_PATH = config.age.secrets.github_token.path;
       };
     };
     neovim = {
