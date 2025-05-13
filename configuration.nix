046522603125ce5be5254eaa9f00593c44809a43
Sean Behan <codebam@riseup.net>
2025-05-13 07:07:02 -0400

    disable age

--- configuration.nix
+++ configuration.nix
@@ -81,11 +81,11 @@
     };
   };
 
-  age = {
-    identityPaths = [ ./secrets/identities/yubikey-5c.txt ./secrets/identities/yubikey-5c-nfc.txt ];
-    secrets.hashedpassword.file = ./secrets/hashedpassword.age;
-    ageBin = "PATH=$PATH:${lib.makeBinPath [pkgs.age-plugin-yubikey]} ${pkgs.rage}/bin/rage";
-  };
+  # age = {
+  #   identityPaths = [ ./secrets/identities/yubikey-5c.txt ./secrets/identities/yubikey-5c-nfc.txt ];
+  #   secrets.hashedpassword.file = ./secrets/hashedpassword.age;
+  #   ageBin = "PATH=$PATH:${lib.makeBinPath [pkgs.age-plugin-yubikey]} ${pkgs.rage}/bin/rage";
+  # };
 
   services = {
     resolved.enable = true;
