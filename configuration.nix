ba8e79676e332821153c90c8f575ddb945fec973
Sean Behan <codebam@riseup.net>
2025-05-26 13:48:29 -0400

    change default clock rate, fixes cs2

--- configuration.nix
+++ configuration.nix
@@ -209,7 +209,7 @@
         (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/10-high-sample-rate.conf" ''
           context.properties = {
             default.clock.allowed-rates = [ 192000 384000 768000 ]
-            default.clock.rate = 768000
+            default.clock.rate = 192000
           }
         '')
       ];
