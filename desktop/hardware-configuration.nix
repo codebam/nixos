a37d7a8fdc7c2be7831de5ad6c5009a69b49a745
Sean Behan <codebam@riseup.net>
2025-05-27 02:51:21 -0400

    backup system openrgb files

--- desktop/hardware-configuration.nix
+++ desktop/hardware-configuration.nix
@@ -62,6 +62,7 @@
       "/var/log"
       "/var/lib/bluetooth"
       "/var/lib/nixos"
+      "/var/lib/OpenRGB"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
       { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
