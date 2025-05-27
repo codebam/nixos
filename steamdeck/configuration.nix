eed505479d38e9c79b2e019306ca90085f33dd86
Sean Behan <codebam@riseup.net>
2025-05-27 00:22:19 -0400

    modify configs

--- steamdeck/configuration.nix
+++ steamdeck/configuration.nix
@@ -1,10 +1,24 @@
 {pkgs, ...}:
 {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+
+  networking = {
+    hostName = "nixos-steamdeck";
+  };
+
   jovian = {
     steam = {
       enable = true;
       autoStart = true;
       desktopSession = "plasma";
     };
+  };
+
+  system = {
+    stateVersion = "23.11";
   };
 }
