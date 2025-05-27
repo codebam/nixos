7fb1ad4b5136869d0b07ae90f7ea056f8cbe67f5
Sean Behan <codebam@riseup.net>
2025-05-26 23:05:00 -0400

    refactor

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -51,7 +51,56 @@
   #   in
   #   pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_next);
 
+  boot = {
+    initrd.systemd = {
+      services = {
+        create-needed-for-boot-dirs = {
+          after = [ "unlock-bcachefs--.service" "cleanup-root.service" ];
+          serviceConfig.KeyringMode = "inherit";
+        };
+        cleanup-root = {
+          unitConfig.DefaultDependencies = false;
+          serviceConfig.Type = "oneshot";
+          serviceConfig.KeyringMode = "inherit";
+          requiredBy = [ "initrd.target" ];
+          after = [
+            "unlock-bcachefs--.service"
+            "local-fs-pre.target"
+          ];
+          before = [ "sysroot.mount" ];
+          script = ''
+            mkdir -p /bcachefs_tmp
+            mount -t bcachefs /dev/disk/by-id/nvme-Sabrent_Rocket_Q_FC6207030D4501357285-part3 /bcachefs_tmp
+            if [[ -e /bcachefs_tmp/@root ]]; then
+                mkdir -p /bcachefs_tmp/old_roots
+                timestamp=$(date --date="@$(stat -c %Y /bcachefs_tmp/@root)" "+%Y-%m-%-d_%H:%M:%S")
+                mv /bcachefs_tmp/@root "/bcachefs_tmp/old_roots/$timestamp"
+            fi
+
+            delete_subvolume_recursively() {
+                IFS=$'\n'
+                for i in $(bcachefs subvolume list -o "$1" | cut -f 2- -d ' '); do
+                    delete_subvolume_recursively "/bcachefs_tmp/$i"
+                done
+                bcachefs subvolume delete "$1"
+            }
+
+            for i in $(find /bcachefs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+                delete_subvolume_recursively "$i"
+            done
+
+            bcachefs subvolume create /bcachefs_tmp/@root
+            umount /bcachefs_tmp
+          '';
+        };
+      };
+    };
+  };
+
   systemd.services = {
+    systemd-remount-fs = {
+      enable = false;
+    };
     applyGpuSettings = {
       description = "Apply GPU Overclocking and Power Limit Settings";
       after = [ "multi-user.target" ];
@@ -81,6 +130,30 @@
   powerManagement.enable = true;
 
   services = {
+    nginx = {
+      enable = true;
+      virtualHosts = {
+        "ai.seanbehan.ca" = {
+          enableACME = true;
+          addSSL = true;
+          locations = {
+            "/" = {
+              proxyPass = "http://localhost:8080/";
+            };
+          };
+        };
+      };
+    };
+    pipewire = {
+      configPackages = [
+        (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/10-high-sample-rate.conf" ''
+          context.properties = {
+            default.clock.allowed-rates = [ 192000 384000 768000 ]
+            default.clock.rate = 192000
+          }
+        '')
+      ];
+    };
     hardware.openrgb = {
       enable = true;
     };
