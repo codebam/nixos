44cc8e8ffe0f5c35161760b67ab8146306c57ff7
Sean Behan <codebam@riseup.net>
2025-05-28 23:16:32 -0400

    add to steam button in plasma

--- steamdeck/configuration.nix
+++ steamdeck/configuration.nix
@@ -16,6 +16,21 @@
     };
   };
 
+  environment.etc."kio/servicemenus/steam.desktop".text = ''
+    [Desktop Entry]
+    Type=Service
+    ServiceTypes=KonqPopupMenu/Plugin
+    MimeType=application/x-desktop;application/x-executable;text/plain;
+    Actions=openInSteam
+    X-KDE-Priority=TopLevel
+    Icon=steam
+
+    [Desktop Action openInSteam]
+    Name=Open with Steam
+    Icon=steam
+    Exec=${pkgs.steam}/bin/steam %u
+  '';
+
   system = {
     stateVersion = "23.11";
   };
