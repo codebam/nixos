d6f2ecaafe145600eec50f0968058ea3c653ff60
Sean Behan <codebam@riseup.net>
2025-06-04 10:21:14 -0400

    Revert "just sleep in the actual service"
    This reverts commit 8c41382b652ed932e493948948d84ac9e8f77230.

--- steamdeck/configuration.nix
+++ steamdeck/configuration.nix
@@ -72,11 +72,22 @@
   systemd = {
     user = {
       services = {
-        steamos-manager = {
+        steamos-manager-restart = {
+          description = "Restart steamos-manager service for codebam on boot";
+          wantedBy = [ "default.target" ];
+          after = [ "steamos-manager.service" ];
           serviceConfig = {
+            Type = "oneshot";
             ExecStartPre = "/run/current-system/sw/bin/sleep 5";
+            ExecStart = "/run/current-system/sw/bin/systemctl --user restart steamos-manager.service";
+            RemainAfterExit = true;
           };
         };
+      };
+    };
+    services = {
+      systemd-remount-fs = {
+        enable = true;
       };
     };
   };
