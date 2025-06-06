82d94b6209f978d28b834e47c06358dcf0ba6cf9
Sean Behan <codebam@riseup.net>
2025-06-05 23:03:35 -0400

    add nixos-desktop.local as a builder

--- steamdeck/configuration.nix
+++ steamdeck/configuration.nix
@@ -6,10 +6,19 @@
     distributedBuilds = true;
     buildMachines = [
       {
+        hostName = "nixos-desktop.local";
+        system = "x86_64-linux,i686-linux";
+        maxJobs = 15;
+        speedFactor = 10;
+        supportedFeatures = [ "big-parallel" ];
+        sshUser = "codebam";
+        sshKey = "/home/codebam/.ssh/id_ed25519";
+      }
+      {
         hostName = "codebam.duckdns.org";
         system = "x86_64-linux,i686-linux";
         maxJobs = 15;
-        speedFactor = 4;
+        speedFactor = 1;
         supportedFeatures = [ "big-parallel" ];
         sshUser = "codebam";
         sshKey = "/home/codebam/.ssh/id_ed25519";
