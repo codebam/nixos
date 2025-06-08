83d15d075700ae07140060d989fcedb1a77e8b98
Sean Behan <codebam@riseup.net>
2025-06-08 00:09:09 -0400

    nixfmt

--- steamdeck/configuration.nix
+++ steamdeck/configuration.nix
@@ -80,9 +80,11 @@
 
   environment.systemPackages = with pkgs; [
     steamdeck-firmware
-    (pkgs.kodi-wayland.withPackages (kodiPkgs: with kodiPkgs; [
-      inputstream-adaptive
-    ]))
+    (pkgs.kodi-wayland.withPackages (
+      kodiPkgs: with kodiPkgs; [
+        inputstream-adaptive
+      ]
+    ))
     protonup-qt
     maliit-keyboard
     maliit-framework
