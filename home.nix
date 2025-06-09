34569bb006921c9429030839506d6b1f5b5f4c38
Sean Behan <codebam@riseup.net>
2025-06-09 10:28:45 -0400

    override the bar font

--- home.nix
+++ home.nix
@@ -119,6 +119,10 @@
             {
               statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-default.toml";
               mode = "dock";
+              fonts = lib.mkForce {
+                names = [ "Fira Code NerdFont" ];
+                size = 11.0;
+              };
               position = "top";
               hiddenState = "hide";
               trayOutput = "none";
