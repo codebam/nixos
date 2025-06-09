81fc88041839098b51b295adf8f595310629ae59
Sean Behan <codebam@riseup.net>
2025-06-09 11:46:41 -0400

    build steamdeck configuration and cache it daily

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -80,7 +80,28 @@
   };
 
   systemd = {
+    timers = {
+      nix-build-steamdeck = {
+        description = "Daily NixOS Build Timer for Steam Deck Configuration";
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnCalendar = "daily";
+          Persistent = true;
+          RandomizedDelaySec = 3600;
+        };
+      };
+    };
     services = {
+      nix-build-steamdeck = {
+        description = "NixOS Build Service for Steam Deck Configuration";
+        after = [ "network.target" ];
+        serviceConfig = {
+          Type = "oneshot";
+          WorkingDirectory = "/etc/nixos/steamdeck-cache";
+          ExecStart = "/run/current-system/sw/bin/nix build /etc/nixos#nixosConfigurations.nixos-steamdeck.config.system.build.toplevel --print-build-logs";
+        };
+        path = [ pkgs.git ];
+      };
       systemd-remount-fs = {
         enable = false;
       };
