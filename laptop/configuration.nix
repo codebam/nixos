1fbf55fd5a0e259a47a40bcfc8677306ced8d9f3
Sean Behan <codebam@riseup.net>
2025-06-10 17:27:40 -0400

    bring laptop up to date

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -10,6 +10,62 @@
   boot = {
     kernelPackages = pkgs.linuxPackages_xanmod_latest;
     supportedFilesystems = [ "bcachefs" ];
+    initrd = {
+      systemd = {
+        services = {
+          create-needed-for-boot-dirs = {
+            after = [
+              "unlock-bcachefs--.service"
+              "cleanup-root.service"
+            ];
+            serviceConfig.KeyringMode = "inherit";
+          };
+          cleanup-root = {
+            unitConfig.DefaultDependencies = false;
+            serviceConfig.Type = "oneshot";
+            serviceConfig.KeyringMode = "inherit";
+            requiredBy = [ "initrd.target" ];
+            after = [
+              "unlock-bcachefs--.service"
+              "local-fs-pre.target"
+            ];
+            before = [ "sysroot.mount" ];
+            script = ''
+              mkdir -p /bcachefs_tmp
+              mount -t bcachefs /dev/disk/by-id/ID-part3 /bcachefs_tmp
+              if [[ -e /bcachefs_tmp/@root ]]; then
+                mkdir -p /bcachefs_tmp/old_roots
+                timestamp=$(date --date="@$(stat -c %Y /bcachefs_tmp/@root)" "+%Y-%m-%-d_%H:%M:%S")
+                mv /bcachefs_tmp/@root "/bcachefs_tmp/old_roots/$timestamp"
+              fi
+
+              delete_subvolume_recursively() {
+                IFS=$'\n'
+                for i in $(bcachefs subvolume list -o "$1" | cut -f 2- -d ' '); do
+                  delete_subvolume_recursively "/bcachefs_tmp/$i"
+                done
+                bcachefs subvolume delete "$1"
+              }
+
+              for i in $(find /bcachefs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+                delete_subvolume_recursively "$i"
+              done
+
+              bcachefs subvolume create /bcachefs_tmp/@root
+              umount /bcachefs_tmp
+            '';
+          };
+        };
+      };
+    };
+  };
+
+  systemd = {
+    services = {
+      systemd-remount-fs = {
+        enable = false;
+      };
+    };
   };
 
   nixpkgs.overlays = [ (final: prev: { }) ];
@@ -17,8 +73,11 @@
   system = {
     autoUpgrade = {
       enable = true;
-      flake = "github:codebam/nixos#nixos-laptop";
-      dates = "09:00";
+      flake = "/etc/nixos";
+      operation = "switch";
+      dates = "daily";
+      randomizedDelaySec = "10min";
+      allowReboot = false;
     };
     stateVersion = "23.11";
   };
--- laptop/hardware-configuration.nix
+++ laptop/hardware-configuration.nix
@@ -10,7 +10,10 @@
 }:
 
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    ./disko.nix
+  ];
 
   boot.initrd.availableKernelModules = [
     "xhci_pci"
@@ -21,27 +24,6 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-
-  fileSystems."/" = {
-    device = "UUID=3541a298-17c9-48dd-8202-c9e0b263d2d4";
-    fsType = "bcachefs";
-    options = [
-      "compression=lz4"
-      "background_compression=zstd"
-      "discard"
-    ];
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/C5ED-F22B";
-    fsType = "vfat";
-    options = [
-      "fmask=0022"
-      "dmask=0022"
-    ];
-  };
-
-  swapDevices = [ { device = "/dev/disk/by-uuid/e53a120f-08de-464c-b46a-4cc4edb0256a"; } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
