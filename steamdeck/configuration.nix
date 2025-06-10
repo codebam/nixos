dc506072f133aec8fa6280ebbbe325f4a35ec496
Sean Behan <codebam@riseup.net>
2025-06-10 14:48:20 -0400

    add gst plugins

--- steamdeck/configuration.nix
+++ steamdeck/configuration.nix
@@ -120,7 +120,14 @@
       enable = true;
       user = "codebam";
       stateDir = "/home/codebam/.config/decky-loader";
-      extraPackages = [ pkgs.gst_all_1.gstreamer ];
+      extraPackages = with pkgs; [
+        gst_all_1.gstreamer
+        gst_all_1.gst-plugins-base
+        gst_all_1.gst-plugins-good
+        gst_all_1.gst-plugins-bad
+        gst_all_1.gst-plugins-ugly
+        gst_all_1.gst-libav
+      ];
     };
     steam = {
       enable = true;
