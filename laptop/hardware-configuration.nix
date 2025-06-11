afc6c10a5dffa91062a7f6d1d84e75c393b3c049
Sean Behan <codebam@riseup.net>
2025-06-10 21:51:10 -0400

    apply linting suggestions

--- laptop/hardware-configuration.nix
+++ laptop/hardware-configuration.nix
@@ -15,15 +15,17 @@
     ./disko.nix
   ];
 
-  boot.initrd.availableKernelModules = [
-    "xhci_pci"
-    "nvme"
-    "usb_storage"
-    "sd_mod"
-  ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    initrd.availableKernelModules = [
+      "xhci_pci"
+      "nvme"
+      "usb_storage"
+      "sd_mod"
+    ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+  };
 
   preservation = {
     enable = true;
