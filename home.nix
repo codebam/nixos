4ba0121d4412358952744c9d7b332abb9068d0bd
Sean Behan <codebam@riseup.net>
2025-06-12 14:05:27 -0400

    manually set wmenu font

--- home.nix
+++ home.nix
@@ -75,7 +75,7 @@
       config = rec {
         inherit modifier;
         terminal = "${pkgs.foot}/bin/foot";
-        menu = "${pkgs.wmenu}/bin/wmenu-run -i -N 1e1e2e -n 89b4fa -M 1e1e2e -m 89b4fa -S 89b4fa -s cdd6f4";
+        menu = "${pkgs.wmenu}/bin/wmenu-run -f \"Fira Code NerdFont 11\" -i -N 1e1e2e -n 89b4fa -M 1e1e2e -m 89b4fa -S 89b4fa -s cdd6f4";
         output = {
           "Dell Inc. Dell AW3821DW #GTIYMxgwABhF" = {
             mode = "3840x1600@143.998Hz";
