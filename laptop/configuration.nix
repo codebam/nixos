6e52a1882bcb9ee06bb60218d95d720de3c7e2a4
Sean Behan <codebam@riseup.net>
2025-06-13 13:11:37 -0400

    remove autoUpgrade from laptop

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -71,14 +71,6 @@
   nixpkgs.overlays = [ (final: prev: { }) ];
 
   system = {
-    autoUpgrade = {
-      enable = true;
-      flake = "/etc/nixos";
-      operation = "switch";
-      dates = "daily";
-      randomizedDelaySec = "10min";
-      allowReboot = false;
-    };
     stateVersion = "25.11";
   };
 }
