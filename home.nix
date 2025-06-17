c4c04b533d40dd90cf0a86c25d214f3e1e43971a
Sean Behan <codebam@riseup.net>
2025-06-17 16:44:29 -0400

    configure blink.cmp

--- home.nix
+++ home.nix
@@ -412,6 +412,48 @@
         require("blink.cmp").setup{
           signature = { enabled = true },
           snippets = { preset = 'luasnip' },
+          keymap = {
+            preset = "enter",
+            ["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
+            ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
+          },
+          ghost_text = {
+            enabled = true,
+          },
+          documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 0,
+          },
+          accept = {
+            auto_brackets = {
+              enabled = true,
+              blocked_filetypes = { "gleam" },
+            },
+          },
+          sources = {
+            transform_items = function(_, items)
+              for _, item in ipairs(items) do
+                if item.kind == require("blink.cmp.types").CompletionItemKind.Snippet then
+                  item.score_offset = item.score_offset + 10
+                end
+              end
+              return items
+            end,
+            default = {
+              "lsp",
+              "path",
+              "snippets",
+              "lazydev",
+              "omni",
+            },
+            providers = {
+              lazydev = {
+                name = "LazyDev",
+                module = "lazydev.integrations.blink",
+                score_offset = 100,
+              },
+            },
+          },
         }
 
         require("avante_lib").load()
@@ -487,6 +529,7 @@
         pkgs.vimPlugins.gitgutter
         pkgs.vimPlugins.telescope-nvim
         pkgs.vimPlugins.lightline-vim
+        pkgs.vimPlugins.lazydev-nvim
         pkgs.vimPlugins.lsp-format-nvim
         pkgs.vimPlugins.luasnip
         pkgs.vimPlugins.blink-cmp
