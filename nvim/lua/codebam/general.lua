943d6071a44a36b749791a5403fbd0b55127ea12
Sean Behan <codebam@riseup.net>
2025-06-18 03:08:17 -0400

    neovim lua modules

---
+++ nvim/lua/codebam/general.lua
@@ -0,0 +1,8 @@
+vim.cmd.colorscheme("catppuccin_mocha")
+
+vim.opt.tabstop = 2
+
+local undodir = vim.fn.stdpath("state") .. "/undo"
+vim.fn.mkdir(undodir, "p")
+vim.opt.undodir = undodir
+vim.opt.undofile = true
