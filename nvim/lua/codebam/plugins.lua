715bf44cfb8217b21b0a251dcd3dfdfdba68b516
Sean Behan <codebam@riseup.net>
2025-06-19 06:58:13 -0400

    configure treesitter textobjects

--- nvim/lua/codebam/plugins.lua
+++ nvim/lua/codebam/plugins.lua
@@ -57,6 +57,94 @@ require("nvim-treesitter.configs").setup({
 	indent = {
 		enable = true,
 	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<C-space>",
+			node_incremental = "<C-space>",
+			scope_incremental = false,
+			node_decremental = "<bs>",
+		},
+	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+
+			keymaps = {
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
+
+				["as"] = "@statement.outer",
+				["is"] = "@statement.inner",
+
+				["al"] = "@loop.outer",
+				["il"] = "@loop.inner",
+
+				["aa"] = "@parameter.outer",
+				["ia"] = "@parameter.inner",
+
+				["a="] = "@assignment.outer",
+				["i="] = "@assignment.inner",
+				["alhs"] = "@assignment.lhs",
+				["arhs"] = "@assignment.rhs",
+			},
+			include_surrounding_whitespace = true,
+		},
+
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				["]f"] = "@function.outer",
+				["]c"] = "@class.outer",
+				["]a"] = "@parameter.inner",
+				["]s"] = "@statement.outer",
+			},
+			goto_next_end = {
+				["]F"] = "@function.outer",
+				["]C"] = "@class.outer",
+				["]A"] = "@parameter.inner",
+				["]S"] = "@statement.outer",
+			},
+			goto_previous_start = {
+				["[f"] = "@function.outer",
+				["[c"] = "@class.outer",
+				["[a"] = "@parameter.inner",
+				["[s"] = "@statement.outer",
+			},
+			goto_previous_end = {
+				["[F"] = "@function.outer",
+				["[C"] = "@class.outer",
+				["[A"] = "@parameter.inner",
+				["[S"] = "@statement.outer",
+			},
+			goto_next_start = {
+				["]m"] = "@comment.outer",
+			},
+		},
+
+		swap = {
+			enable = true,
+			swap_next = {
+				["<leader>na"] = "@parameter.inner",
+				["<leader>nf"] = "@function.outer",
+			},
+			swap_previous = {
+				["<leader>pa"] = "@parameter.inner",
+				["<leader>pf"] = "@function.outer",
+			},
+		},
+		lsp_interop = {
+			enable = true,
+			peek_definition_code = {
+				["<leader>df"] = "@function.outer",
+				["<leader>dF"] = "@class.outer",
+			},
+		},
+	},
 })
 
 require("conform").setup({
@@ -72,8 +160,6 @@ require("conform").setup({
 		lsp_format = "fallback",
 	},
 })
-
-require("flash").setup()
 
 require("oil").setup()
 
