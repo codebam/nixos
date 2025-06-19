efd7b0a5051123cf2f9f75e01b5c2b910ad7cc43
Sean Behan <codebam@riseup.net>
2025-06-19 16:51:22 -0400

    always use block cursor

--- nvim/lua/codebam/general.lua
+++ nvim/lua/codebam/general.lua
@@ -2,6 +2,8 @@ vim.cmd.colorscheme("catppuccin_mocha")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
+vim.opt.guicursor = "n-v-c-i:block"
+
 vim.opt.tabstop = 2
 
 local undodir = vim.fn.stdpath("state") .. "/undo"
