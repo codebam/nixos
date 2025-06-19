a826d1fd54a6677797846070caf3fbc523531893
Sean Behan <codebam@riseup.net>
2025-06-19 03:33:07 -0400

    add podman-compose

--- configuration/environment.nix
+++ configuration/environment.nix
@@ -2,23 +2,24 @@
 {
   environment = {
     systemPackages = with pkgs; [
-      run0-sudo-shim
-      nushell
-      uutils-coreutils-noprefix
-      uutils-findutils
+      dig
       distrobox
       git
       gparted
       libnotify
       mangohud
+      neovim
       nix-output-monitor
+      nushell
+      podman-compose
       rclone
+      run0-sudo-shim
+      uutils-coreutils-noprefix
+      uutils-findutils
+      via
       virt-manager
       wl-clipboard
       xdg-utils
-      via
-      dig
-      neovim
       (inputs.agenix.packages.${pkgs.system}.default.override {
         ageBin = "${pkgs.rage}/bin/rage";
       })
