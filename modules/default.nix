8ce01070ebc9392da0bb2a38f2239d5807fe33c8
Sean Behan <codebam@riseup.net>
2025-06-25 21:09:45 -0400

    refactor: modular

---
+++ modules/default.nix
@@ -0,0 +1,25 @@
+{
+  imports = [
+    ./hardware/default.nix
+
+    ./system/boot.nix
+    ./system/default.nix
+    ./system/environment.nix
+    ./system/fonts.nix
+    ./system/networking.nix
+    ./system/nix.nix
+    ./system/nixpkgs.nix
+    ./system/preservation.nix
+    ./system/systemd.nix
+    ./system/time.nix
+    ./system/virtualisation.nix
+    ./system/xdg.nix
+    ./system/zram.nix
+
+    ./programs/default.nix
+    ./services/default.nix
+    ./security/default.nix
+    ./users/default.nix
+    ./stylix/default.nix
+  ];
+}
