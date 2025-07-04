5724a9b4387e758f75716d2f54be00bccd5f0b01
Sean Behan <codebam@riseup.net>
2025-07-04 03:00:09 -0400

    add truecolor support to tmux systemd service

--- home/systemd.nix
+++ home/systemd.nix
@@ -10,6 +10,10 @@
           };
           Service = {
             Type = "forking";
+            Environment = [
+              "TERM=xterm-256color"
+              "COLORTERM=truecolor"
+            ];
             ExecStart = "${pkgs.tmux}/bin/tmux -S /run/user/%U/tmux-%U/default new-session -d -s default";
             ExecStop = "${pkgs.tmux}/bin/tmux -S /run/user/%U/tmux-%U/default kill-session -t default";
             Restart = "always";
