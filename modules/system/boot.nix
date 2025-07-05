1a54fd1a2cf564d90d63a6046f10c39297cc2c22
Sean Behan <codebam@riseup.net>
2025-07-05 18:04:09 -0400

    system recovery password

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -4,6 +4,7 @@ _:
   boot = {
     initrd.systemd = {
       enable = true;
+      emergencyAccess = "$6$TIP8YR83obmkq8T2$T3lYdPbPj9wysMznNlS5J0qHo2eyTr43aF/ZWSMWHdNRob4dkBB0s3KpBLUgYRTyPZxbb1ZgeqCrrx.DEEkQX1";
     };
     loader = {
       systemd-boot = {
