571464a816059444adf9384736758eb6b078b4ad
Sean Behan <codebam@riseup.net>
2025-07-07 18:37:19 -0400

    stylix cleanup

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -4,22 +4,11 @@
     enable = true;
     polarity = "dark";
     targets = {
-      chromium.enable = false;
       console.enable = false;
-      feh.enable = false;
       fish.enable = false;
-      glance.enable = false;
       gnome.enable = true;
-      grub.enable = false;
       gtk.enable = true;
-      kmscon.enable = false;
-      lightdm.enable = false;
-      nixvim.enable = false;
-      nvf.enable = false;
-      plymouth.enable = false;
       qt.enable = true;
-      regreet.enable = false;
-      spicetify.enable = false;
     };
     base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
     fonts = {
