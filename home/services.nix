c9aed39981b57bad432d7865360055dc7800bafd
Sean Behan <codebam@riseup.net>
2025-07-08 13:48:38 -0400

    move open-webui to desktop home

--- home/services.nix
+++ home/services.nix
@@ -8,27 +8,5 @@ _:
         layer = "overlay";
       };
     };
-    podman = {
-      enable = true;
-      containers = {
-        open-webui = {
-          autoStart = true;
-          autoUpdate = "registry";
-          description = "open-webui container";
-          image = "ghcr.io/open-webui/open-webui:main";
-          ports = [
-            "8080:8080"
-          ];
-          volumes = [
-            "open-webui-data:/app/backend/data"
-          ];
-          environment = {
-            ENV = "prod";
-            OLLAMA_BASE_URL = "http://host.containers.internal:11434";
-            SEARXNG_QUERY_URL = "http://host.containers.internal:8081/search?q=<query>";
-          };
-        };
-      };
-    };
   };
 }
