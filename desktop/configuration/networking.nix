5a35ebd423078e41a9a8db22ebf1d5819fec84d9
Sean Behan <codebam@riseup.net>
2025-07-30 19:39:19 -0400

    add palworld ports through firewall

--- desktop/configuration/networking.nix
+++ desktop/configuration/networking.nix
@@ -3,5 +3,12 @@ _:
 {
   networking = {
     hostName = "nixos-desktop";
+    firewall.allowedTCPPorts = [
+      25575 # RCON port
+    ];
+    firewall.allowedUDPPorts = [
+      8211 # PalWorld port
+      27015 # Steam query port
+    ];
   };
 }
