f7636073a6a6a702c500df22b0f946e2972b5c4f
Sean Behan <codebam@riseup.net>
2025-08-01 12:37:32 -0400

    more ports

--- desktop/configuration/networking.nix
+++ desktop/configuration/networking.nix
@@ -5,6 +5,7 @@ _:
     hostName = "nixos-desktop";
     firewall.allowedTCPPorts = [
       25575 # RCON port
+      8212 # PalWorld
     ];
     firewall.allowedUDPPorts = [
       8211 # PalWorld port
