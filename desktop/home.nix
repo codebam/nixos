9b7d7dc7ad8a6ca8a8b3a5746b1cef0a547da922
Sean Behan <codebam@riseup.net>
2025-10-26 21:56:20 -0400

    new gpg key. lol

--- desktop/home.nix
+++ desktop/home.nix
@@ -83,7 +83,7 @@
   programs = {
     git = {
       signing = {
-        key = "0F6D5021A87F92BA";
+        key = "0271B12CCF0A185B01EB25FA4B1C30CAAB93976B";
         signByDefault = true;
       };
     };
