61e7675813a4680395b407d7d2d903c8a1708b38
Sean Behan <codebam@riseup.net>
2025-10-31 14:31:19 -0400

    steel-ls

--- home/home.nix
+++ home/home.nix
@@ -113,6 +113,10 @@
       (require-builtin steel/random as rand::)
       (require (prefix-in helix. "helix/commands.scm"))
       (require (prefix-in helix.static. "helix/static.scm"))
+      (require "helix/configuration.scm")
+      (define-lsp "steel-language-server" (command "steel-language-server") (args '()))
+      (define-language "scheme"
+                       (language-servers '("steel-language-server")))
       (require "scooter/scooter.scm")
       (require "helixwiki/main.scm")
       (wiki-set-path! "~/Documents/wiki")
