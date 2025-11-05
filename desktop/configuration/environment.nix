ee6a89cc6caf9a034f28a1849d81fddaa0252a95
Sean Behan <codebam@riseup.net>
2025-11-04 19:57:47 -0500

    update & remove blender-hip

--- desktop/configuration/environment.nix
+++ desktop/configuration/environment.nix
@@ -4,7 +4,7 @@
   environment.systemPackages = with pkgs; [
     cargo
     gcc
-    blender-hip
+    # blender-hip
     inputs.steel.packages.${pkgs.stdenv.hostPlatform.system}.default
     inputs.lulu.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
