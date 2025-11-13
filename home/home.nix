de0ad252e3d9985095917cdcb9d2ed1041f358fa
Sean Behan <codebam@riseup.net>
2025-11-13 01:26:55 -0500

    disable stupid proton stuffs

--- home/home.nix
+++ home/home.nix
@@ -25,12 +25,12 @@
     sessionVariables = {
       EDITOR = "hx";
       NIXOS_OZONE_WL = "1";
-      OBS_VKCAPTURE = "1";
+      # OBS_VKCAPTURE = "1";
       # WLR_RENDERER = "vulkan";
       MANGOHUD_CONFIGFILE = "/home/codebam/.config/MangoHud/MangoHud.conf";
-      PROTON_ENABLE_WAYLAND = "1";
-      PROTON_ENABLE_HDR = "1";
-      PROTON_USE_NTSYNC = "1";
+      # PROTON_ENABLE_WAYLAND = "1";
+      # PROTON_ENABLE_HDR = "1";
+      # PROTON_USE_NTSYNC = "1";
       SEARXNG_API_URL = "http://localhost:8081";
     };
 
