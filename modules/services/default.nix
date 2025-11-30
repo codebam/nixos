0f2ae043117022873c3c9f4926d7912980580ae0
Sean Behan <codebam@riseup.net>
2025-11-30 16:28:47 -0500

    add rules for new keyboard

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -34,6 +34,11 @@
       ];
       extraRules = ''
         KERNEL=="ntsync", MODE="0660", TAG+="uaccess"
+
+        SUBSYSTEM!="usb_device", ACTION!="add", GOTO="objdev_rules_end"
+        ATTRS{idVendor}=="1f3a", ATTRS{idProduct}=="efe8", MODE="0666"
+        LABEL="objdev_rules_end"
+        SUBSYSTEM=="input", SYMLINK=="input/by-path/*event-mouse", GROUP="users", MODE="0660"
       '';
     };
     desktopManager.plasma6.enable = true;
