43822532ac05effd13d71672421f7a4e0f471141
Sean Behan <codebam@riseup.net>
2025-12-09 02:06:10 -0500

    re-enable suspend

--- desktop/configuration/systemd.nix
+++ desktop/configuration/systemd.nix
@@ -4,24 +4,24 @@
   powerManagement.enable = true;
 
   systemd = {
-    targets = {
-      sleep = {
-        enable = false;
-        unitConfig.DefaultDependencies = "no";
-      };
-      suspend = {
-        enable = false;
-        unitConfig.DefaultDependencies = "no";
-      };
-      hibernate = {
-        enable = false;
-        unitConfig.DefaultDependencies = "no";
-      };
-      "hybrid-sleep" = {
-        enable = false;
-        unitConfig.DefaultDependencies = "no";
-      };
-    };
+    # targets = {
+    #   sleep = {
+    #     enable = false;
+    #     unitConfig.DefaultDependencies = "no";
+    #   };
+    #   suspend = {
+    #     enable = false;
+    #     unitConfig.DefaultDependencies = "no";
+    #   };
+    #   hibernate = {
+    #     enable = false;
+    #     unitConfig.DefaultDependencies = "no";
+    #   };
+    #   "hybrid-sleep" = {
+    #     enable = false;
+    #     unitConfig.DefaultDependencies = "no";
+    #   };
+    # };
     timers = {
       nix-build-steamdeck = {
         enable = false;
