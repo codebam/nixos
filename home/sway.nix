f74cd47e8bbf19437aaa98e8e61f77a2e25d80ae
Sean Behan <codebam@riseup.net>
2025-12-09 00:32:16 -0500

    disable workspace assigns

--- home/sway.nix
+++ home/sway.nix
@@ -149,17 +149,7 @@
             "XF86MonBrightnessUp" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set +1%";
             "XF86MonBrightnessDown" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 1%-";
           };
-        assigns = {
-          "1" = [
-            { app_id = "^org\.telegram\.desktop$"; }
-            { app_id = "^discord$"; }
-            { app_id = "^Element$"; }
-          ];
-          "2" = [
-            { app_id = "^librewolf$"; }
-          ];
-          "5" = [{ app_id = "^com\.github\.th_ch\.youtube_music$"; }];
-        };
+        assigns = {};
       };
       extraConfig =
         let
