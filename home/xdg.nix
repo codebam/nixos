ff8fb7cc739329fcea245799e90516f54f820c04
Sean Behan <codebam@riseup.net>
2025-12-12 11:01:51 -0500

    set default browser

--- home/xdg.nix
+++ home/xdg.nix
@@ -3,5 +3,14 @@ _:
 {
   xdg = {
     enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "chromium-browser.desktop";
+        "x-scheme-handler/http"  = "chromium-browser.desktop";
+        "x-scheme-handler/https" = "chromium-browser.desktop";
+        "x-scheme-handler/about" = "chromium-browser.desktop";
+      };
+    };
   };
 }
