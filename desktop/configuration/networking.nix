c3cc8c81dd7578a45d99e1be1c3c87c5fbf79740
Sean Behan <codebam@riseup.net>
2026-01-04 11:03:37 -0500

    use cloudflare timeservers, perf tweaks

--- desktop/configuration/networking.nix
+++ desktop/configuration/networking.nix
@@ -2,6 +2,11 @@ _:
 
 {
   networking = {
+    timeServers = [
+      "time.cloudflare.com"
+      "time.google.com"
+      "0.ca.pool.ntp.org"
+    ];
     hostName = "nixos-desktop";
     firewall.allowedTCPPorts = [
       25575 # RCON port
