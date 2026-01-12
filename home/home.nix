d69331405082296bda34001e079076a0f213b80b
Sean Behan <codebam@riseup.net>
2026-01-12 05:28:13 -0500

    fix chromium

--- home/home.nix
+++ home/home.nix
@@ -29,6 +29,7 @@
       WLR_RENDERER = "vulkan";
       AMD_USERQ = "1";
       RADV_PERFTEST = "userq";
+      GTK_USE_PORTAL = "1";
       # WLR_DRM_NO_ATOMIC = "1"; # screen tearing support
       # MANGOHUD_CONFIGFILE = "/home/codebam/.config/MangoHud/MangoHud.conf";
       # PROTON_ENABLE_WAYLAND = "1";
