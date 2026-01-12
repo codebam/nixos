ca0290957fa1cd56844c9967854d99b0454aaf86
Sean Behan <codebam@riseup.net>
2026-01-12 05:28:13 -0500

    fix chromium

--- home/xdg.nix
+++ home/xdg.nix
@@ -6,10 +6,10 @@ _:
     mimeApps = {
       enable = true;
       defaultApplications = {
-        "text/html" = "chromium-browser.desktop";
-        "x-scheme-handler/http"  = "chromium-browser.desktop";
-        "x-scheme-handler/https" = "chromium-browser.desktop";
-        "x-scheme-handler/about" = "chromium-browser.desktop";
+        "text/html" = "chromium.desktop";
+        "x-scheme-handler/http"  = "chromium.desktop";
+        "x-scheme-handler/https" = "chromium.desktop";
+        "x-scheme-handler/about" = "chromium.desktop";
       };
     };
   };
--- modules/system/xdg.nix
+++ modules/system/xdg.nix
@@ -5,6 +5,7 @@
     portal = {
       config.common.default = "gtk";
       enable = true;
+      xdgOpenUsePortal = true;
       wlr.enable = true;
       extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
     };
