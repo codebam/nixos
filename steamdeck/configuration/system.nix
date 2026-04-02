3466f461780b6853dbf2ea028d8dd1ebd7cd4a86
Sean Behan <codebam@riseup.net>
2026-04-02 11:03:51 -0400

    steamdeck fix

--- steamdeck/configuration/system.nix
+++ steamdeck/configuration/system.nix
@@ -4,5 +4,5 @@ _:
   system = {
     stateVersion = "26.05";
   };
-  systemd.user.services.steamos-manager.after = [ "cecd.service" ];
+  # systemd.user.services.steamos-manager.after = [ "cecd.service" ];
 }
