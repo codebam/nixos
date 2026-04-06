93e383016f51030c53aef0b3e64896c53d79ff2c
Sean Behan <codebam@riseup.net>
2026-04-06 18:33:26 -0400

    avf try fix

--- avf/configuration.nix
+++ avf/configuration.nix
@@ -4,7 +4,7 @@
   boot.lanzaboote.enable = lib.mkForce false;
   networking.hostName = "nixos-avf";
   networking.useDHCP = lib.mkForce true;
-  networking.networkmanager.enable = lib.mkForce false;
+  hardware.graphics.enable32Bit = lib.mkForce false;
   nixpkgs.hostPlatform = "aarch64-linux";
   avf.defaultUser = "codebam";
   system.stateVersion = "26.05";
