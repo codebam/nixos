6fb7af8c3118dffe2a59463e00649559793d12f1
Sean Behan <codebam@riseup.net>
2026-04-06 18:28:42 -0400

    avf

--- avf/configuration.nix
+++ avf/configuration.nix
@@ -3,6 +3,7 @@
 {
   boot.lanzaboote.enable = lib.mkForce false;
   networking.hostName = "nixos-avf";
+  networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = "aarch64-linux";
   avf.defaultUser = "codebam";
   system.stateVersion = "26.05";
