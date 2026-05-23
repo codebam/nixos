d698bb2404755946c4c6be63c29ca3ac47176b46
Sean Behan <codebam@riseup.net>
2026-05-23 02:48:59 -0400

    vpn fixes

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -44,8 +44,8 @@
         Resolve = {
           DNS = "1.1.1.1#cloudflare-dns.com 9.9.9.9#dns.quad9.net";
           FallbackDNS = "8.8.8.8#dns.google";
-          DNSSEC = "true";
-          DNSOverTLS = "true";
+          DNSSEC = "allow-downgrade";
+          DNSOverTLS = "opportunistic";
         };
       };
     };
