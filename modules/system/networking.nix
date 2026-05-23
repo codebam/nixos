41ac0bce296b9a2e4be6ddc763ce32adf45d117b
Sean Behan <codebam@riseup.net>
2026-05-22 23:43:45 -0400

    firewall bypass

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}:
+{ pkgs, config, lib, ... }:
 {
   networking = {
     networkmanager = {
@@ -24,6 +24,30 @@
     };
     nftables = {
       enable = true;
+      tables = {
+        vpn-bypass = {
+          family = "ip";
+          content = let
+            tcpPorts = builtins.concatStringsSep ", " (map toString config.networking.firewall.allowedTCPPorts);
+            udpPorts = builtins.concatStringsSep ", " (map toString config.networking.firewall.allowedUDPPorts);
+            tcpRanges = builtins.concatStringsSep ", " (map (r: "${toString r.from}-${toString r.to}") config.networking.firewall.allowedTCPPortRanges);
+            udpRanges = builtins.concatStringsSep ", " (map (r: "${toString r.from}-${toString r.to}") config.networking.firewall.allowedUDPPortRanges);
+          in ''
+            chain prerouting {
+              type filter hook prerouting priority mangle; policy accept;
+              ${lib.optionalString (tcpPorts != "") ''iifname { "wlan0", "enp6s0" } tcp dport { ${tcpPorts} } ct state new ct mark set 0xca6c''}
+              ${lib.optionalString (udpPorts != "") ''iifname { "wlan0", "enp6s0" } udp dport { ${udpPorts} } ct state new ct mark set 0xca6c''}
+              ${lib.optionalString (tcpRanges != "") ''iifname { "wlan0", "enp6s0" } tcp dport { ${tcpRanges} } ct state new ct mark set 0xca6c''}
+              ${lib.optionalString (udpRanges != "") ''iifname { "wlan0", "enp6s0" } udp dport { ${udpRanges} } ct state new ct mark set 0xca6c''}
+              ct mark 0xca6c meta mark set 0xca6c
+            }
+            chain output {
+              type route hook output priority mangle; policy accept;
+              ct mark 0xca6c meta mark set 0xca6c
+            }
+          '';
+        };
+      };
     };
     firewall = rec {
       enable = true;
