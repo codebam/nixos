b16731f1a59cefb67b27c43e2cbc6dca1ecbd0c9
Sean Behan <codebam@riseup.net>
2026-05-25 13:40:38 -0400

    disable searx

--- desktop/configuration/services.nix
+++ desktop/configuration/services.nix
@@ -733,7 +733,7 @@
       };
     };
     searx = {
-      enable = true;
+      enable = false;
       environmentFile = config.age.secrets.searx-secret.path;
       settings = {
         server = {
