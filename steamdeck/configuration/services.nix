0821b4312081363b89d2be6d4606b27eaeaa57c0
Sean Behan <codebam@riseup.net>
2026-06-03 23:57:09 -0400

    fix

--- steamdeck/configuration/services.nix
+++ steamdeck/configuration/services.nix
@@ -1,7 +1,11 @@
-{lib, ...}:
+{ lib, ... }:
 
 {
   services = {
     desktopManager.plasma6.enable = lib.mkForce true;
+    scx = {
+      enable = true;
+      scheduler = lib.mkForce "scx_lavd";
+    };
   };
 }
