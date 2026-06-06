da6a8fa0e342eba295ce93766bf0b97d412fb1d9
Sean Behan <codebam@riseup.net>
2026-06-06 14:26:59 -0400

    disable baloo

--- home/plasma.nix
+++ home/plasma.nix
@@ -9,11 +9,10 @@ _:
         TerminalService = "foot.desktop";
       };
     };
-    fileSearch = {
-      enable = false;
-    };
     configFile = {
+      baloofilerc."Basic Settings".Indexing-Enabled = false;
       kcminputrc."Libinput/13364/832/Keychron Keychron V4 Mouse".PointerAccelerationProfile = 1;
+      kcminputrc."Libinput/14096/21510/Pulsar 8K Dongle Gen.2".PointerAccelerationProfile = 1;
       kwinrc.EdgeBarrier.CornerBarrier = false;
       kwinrc.EdgeBarrier.EdgeBarrier = 0;
       kwinrc.Plugins.hidecursorEnabled = true;
