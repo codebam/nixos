560ec6263eeb98424644fc19d2a398a12716b805
Sean Behan <codebam@riseup.net>
2026-06-30 10:43:44 -0400

    disable pointer accel

--- home/plasma.nix
+++ home/plasma.nix
@@ -12,6 +12,7 @@ _:
     configFile = {
       baloofilerc."Basic Settings".Indexing-Enabled = false;
       kcminputrc."Libinput/13364/832/Keychron Keychron V4 Mouse".PointerAccelerationProfile = 1;
+      kcminputrc."Libinput/14096/13332/Pulsar Pulsar X2 CrazyLight".PointerAccelerationProfile = 1;
       kcminputrc."Libinput/14096/21510/Pulsar 8K Dongle Gen.2".PointerAccelerationProfile = 1;
       kwinrc.EdgeBarrier.CornerBarrier = false;
       kwinrc.EdgeBarrier.EdgeBarrier = 0;
