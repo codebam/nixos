9a8ef47e179d236323b83837760317afb594f27b
Sean Behan <codebam@riseup.net>
2026-07-02 16:26:37 -0400

    disable noisetorch

--- modules/system/systemd.nix
+++ modules/system/systemd.nix
@@ -3,21 +3,21 @@
   systemd = {
     user = {
       services = {
-        noisetorch = {
-          description = "NoiseTorch Noise Cancelling Daemon";
-          wantedBy = [ "graphical-session.target" ];
-          after = [
-            "pipewire.service"
-            "pulseaudio.service"
-          ]; # Ensure audio is running first
+        # noisetorch = {
+        #   description = "NoiseTorch Noise Cancelling Daemon";
+        #   wantedBy = [ "graphical-session.target" ];
+        #   after = [
+        #     "pipewire.service"
+        #     "pulseaudio.service"
+        #   ]; # Ensure audio is running first
 
-          serviceConfig = {
-            Type = "simple";
-            ExecStart = "${pkgs.noisetorch}/bin/noisetorch -i";
-            Restart = "on-failure";
-            RestartSec = 3;
-          };
-        };
+        #   serviceConfig = {
+        #     Type = "simple";
+        #     ExecStart = "${pkgs.noisetorch}/bin/noisetorch -i";
+        #     Restart = "on-failure";
+        #     RestartSec = 3;
+        #   };
+        # };
         pipewire-pulse.environment = {
           LADSPA_PATH = "/tmp:/run/current-system/sw/lib/ladspa";
         };
