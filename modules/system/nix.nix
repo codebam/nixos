d29baf5f730985222ba46e3fe8b039a684f96589
Sean Behan <codebam@riseup.net>
2026-07-21 01:50:25 -0400

    cleanup

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -20,19 +20,10 @@
       cores = 0;
       extra-sandbox-paths = [ config.programs.ccache.cacheDir ];
       builders-use-substitutes = true;
-      substituters = [
-        # "https://cache.nixos.org/"
-        # "https://jovian.cachix.org"
-      ];
-      trusted-public-keys = [
-        # "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        # "jovian.cachix.org-1:8Vq4Txku6VZIRhYrHYki3Ab9XHJRoWmdYqMqj4rB/Uc="
-      ];
     };
     gc = {
       automatic = false;
     };
-    # Enable automatic optimization of the store
     optimise = {
       automatic = true;
       dates = [ "weekly" ];
