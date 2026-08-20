ea5f4d8ff5517f53c01c54d63a134110064808d6
Sean Behan <codebam@riseup.net>
2026-08-20 14:49:03 -0400

    firefox-nightly default browser

--- home/xdg.nix
+++ home/xdg.nix
@@ -42,10 +42,10 @@ in
     mimeApps = {
       enable = true;
       defaultApplications = {
-        "text/html" = "chromium.desktop";
-        "x-scheme-handler/http" = "chromium.desktop";
-        "x-scheme-handler/https" = "chromium.desktop";
-        "x-scheme-handler/about" = "chromium.desktop";
+        "text/html" = "firefox-nightly.desktop";
+        "x-scheme-handler/http" = "firefox-nightly.desktop";
+        "x-scheme-handler/https" = "firefox-nightly.desktop";
+        "x-scheme-handler/about" = "firefox-nightly.desktop";
         "inode/directory" = "rio-folder.desktop";
       };
     };
