6d16cf75fb0f43164cd48568b295a167a1a5620a
Sean Behan <codebam@riseup.net>
2026-08-27 20:35:09 -0400

    default browser chromium

--- home/xdg.nix
+++ home/xdg.nix
@@ -42,10 +42,10 @@ in
     mimeApps = {
       enable = true;
       defaultApplications = {
-        "text/html" = "firefox-nightly.desktop";
-        "x-scheme-handler/http" = "firefox-nightly.desktop";
-        "x-scheme-handler/https" = "firefox-nightly.desktop";
-        "x-scheme-handler/about" = "firefox-nightly.desktop";
+        "text/html" = "chromium.desktop";
+        "x-scheme-handler/http" = "chromium.desktop";
+        "x-scheme-handler/https" = "chromium.desktop";
+        "x-scheme-handler/about" = "chromium.desktop";
         "inode/directory" = "rio-folder.desktop";
       };
     };
